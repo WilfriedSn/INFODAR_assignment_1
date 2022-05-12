3fcb096d063dcc94a60958c66298c22f75e45c5b
Wilfried U <Wilfried.snijders@gmail.com>
2022-05-12 12:24:38 +0200

    ADDED a function to make CEQ readable

--- INFODAR1Main.py
+++ INFODAR1Main.py
@@ -1,5 +1,8 @@
+import re
 import sqlite3
-import os.path # for createSQLITEDB
+import os.path
+from tkinter.tix import Select
+from traceback import format_exc # for createSQLITEDB
 
 #returns all lines from a file
 def readFile(fileName):
@@ -52,7 +55,31 @@ def openSQLITEDB(filename):
 #probaly not needed
 def closeSQLITEDB(con):
     con.close()
-    
+
+#transform CEQ to SQLITEcode
+def transformCEQ(line, table):
+    #split line by ","    
+    result = []
+    line = line.split(",")
+    #check if k is predefined or not, add 
+    if line[0][0] == "k":
+        result[0] = (line[0].remove("k = ")).toInt()
+        #get the rest of line combined
+        line[0] = ""
+        for i in range(1, len(line) - 1):
+            line[0] += line[i] + " AND"
+        line[0] += line[i]
+    else:
+        result[0] = 10
+        for i in range(0, len(line) - 1):
+            line[0] += line[i] + " AND"
+        line[0] += line[i]
+    result[1] = "SELECT * FROM {table} WHERE {line[0]}"
+    return result
+
+
+
+
 createSQLITEDB("test.db")
 print(getSqliteInsertCode("test.db"))
 
