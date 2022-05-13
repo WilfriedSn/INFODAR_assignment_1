948eb4a4ab4b2fee7dee46210d161e1deb13bb9a
Wilfried <WIlfried.snijders@gmail.com>
2022-05-13 15:24:59 +0200

    removed stupid errors

--- INFODAR1Main.py
+++ INFODAR1Main.py
@@ -1,8 +1,8 @@
 import re
 import sqlite3
-import os.path
+import os.path # for createSQLITEDB
 from tkinter.tix import Select
-from traceback import format_exc # for createSQLITEDB
+from traceback import format_exc 
 
 #returns all lines from a file
 def readFile(fileName):
@@ -58,10 +58,10 @@ def closeSQLITEDB(con):
 
 #transform CEQ to SQLITEcode
 def transformCEQ(line, table):
-    #split line by "," 
     k = 10
+    #split line by "," 
     line = line.split(",")
-    #check if k is predefined or not, add 
+    #check if k is predefined or not
     if line[0][0] == "k":
         k = int(line[0][4:])
         #get the rest of line combined
@@ -76,11 +76,3 @@ print(transformCEQ("k = 9, model = ding, year = 1999, price = 50", "table"))
 
 #createSQLITEDB("test.db")
 #print(getSqliteInsertCode("test.db"))
-
-
-
-
-    
-    
-        
-
