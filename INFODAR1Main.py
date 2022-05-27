ce6c1d7fe7856d3205ef38e1b3b34065335373a9
Wilfried S <Wilfried.snijders@gmail.com>
2022-05-27 16:18:08 +0200

    atempt on db3

--- INFODAR1Main.py
+++ INFODAR1Main.py
@@ -1,6 +1,7 @@
 from multiprocessing import connection
 import sqlite3
-import os.path # for createSQLITEDB
+import os.path
+from unittest import result # for createSQLITEDB
 
 #returns all lines from a file
 def readFile(fileName):
@@ -10,97 +11,135 @@ def readFile(fileName):
 
 
 def getSqliteInsertCode(filename):
-    lines = readFile(filename)
-    newLines = []
-    for line in lines:
-        #make new list with all non empty lines
-        if line != "":
-            newLines.append(line)
-    return newLines
+	lines = readFile(filename)
+	newLines = []
+	for line in lines:
+		#make new list with all non empty lines
+		if line != "":
+			newLines.append(line)
+	return newLines
 
 #returns a sqliteconnection to use
 def createSQLITEDB(filename):
-    #check if file exists in the current directory
-    if os.path.exists(filename):
-        print(f'DBFile {filename} got deleted and recreated')
-        os.remove(filename)
-    return sqlite3.connect(filename)
+	#check if file exists in the current directory
+	if os.path.exists(filename):
+		print(f'DBFile {filename} got deleted and recreated')
+		os.remove(filename)
+	return sqlite3.connect(filename)
 
 def openSQLITEDB(filename):
-    #not?, ! doesnt work
-    if not os.path.exists(filename):
-        print(f'dbfile {filename} did not exsist, one was created')
-    return sqlite3.connect(filename)
+	#not?, ! doesnt work
+	if not os.path.exists(filename):
+		print(f'dbfile {filename} did not exsist, one was created')
+	return sqlite3.connect(filename)
 
 
 #probaly not needed
 def closeSQLITEDB(con):
-    con.close()
+	con.close()
 
 #transform CEQ to SQLITEcode
 def transformCEQ(line, table):
-    k = 10
-    #split line by "," 
-    line = line.split(",")
-    #check if k is predefined or not
-    if line[0][0] == "k":
-        k = int(line[0][4:])
-        #get the rest of line combined
-        andQuery = " AND ".join(line[1:])
-    else:
-        andQuery = " AND ".join(line)
-    result = (k, f"SELECT * FROM {table} WHERE {andQuery}")
-    return result
+	k = 10
+	#split line by "," 
+	line = line.split(",")
+	#check if k is predefined or not
+	if line[0][0] == "k":
+		k = int(line[0][4:])
+		#get the rest of line combined
+		andQuery = " AND ".join(line[1:])
+	else:
+		andQuery = " AND ".join(line)
+	result = (k, f"SELECT * FROM {table} WHERE {andQuery}")
+	return result
+
+#transforms workload to a usable 2d list
+def transFormWorkload(fileName):
+	lines = readFile(fileName)
+	workload = list()
+	for line in lines:
+		#check if it is intended as code
+		if ' times: ' in line:
+			#remove trailing spaces and \n, add semicolon make the second part runnable sqlitecode,
+			#   split by " times: " 
+			#   first index (amount of times) as a int
+			#   second index (the Sqlite code) as a string
+			line = line.strip()
+			line += ';'
+			line = line.split(' times: ')
+			workload.append([int(line[0]), line[1]])
+	return workload
 
 #|----------------------------------------------------------------------------------------------------------------------|
 #| db4. Voor elke auto, kijk op hoeveel van de workload queries de auto applied, sla een lookup van auto naar dit op.   |
 #|----------------------------------------------------------------------------------------------------------------------|
 
-#transforms workload to a usable 2d list
-def transFormWorkload(fileName):
-    lines = readFile(fileName)
-    workload = list()
-    for line in lines:
-        #check if it is intended as code
-        if ' times: ' in line:
-            #remove trailing spaces and \n, add semicolon make the second part runnable sqlitecode,
-            #   split by " times: " 
-            #   first index (amount of times) as a int
-            #   second index (the Sqlite code) as a string
-            line = line.strip()
-            line += ';'
-            line = line.split(' times: ')
-            whereClause = line[1].split('FROM autompg')[1]
-            whereClause = "SELECT ID FROM autompg" + whereClause
-            workload.append((int(line[0]), whereClause))
-    return workload
+def workLoadToDB4(workload):
+	for line in workload:
+		whereClause = line[1].split('FROM autompg')[1]
+		line[1] = "SELECT ID FROM autompg" + whereClause
+	return workload
 
 #get ids from sqlite querry
 def getIdsFormDB(query, connection):
-    cursor = connection.cursor()
-    cursor.execute(query)
-    ids = cursor.fetchall()
-    return ids
+	cursor = connection.cursor()
+	cursor.execute(query)
+	ids = cursor.fetchall()
+	return ids
 
 def fillDB4WorkloadData(filename, connection):
-    workload = transFormWorkload(filename)
-    #create a dictionairy <ID,value>
-    workloadArray = [0]
-    #add 500 0 to workloadArray //shouldbechanged
-    for i in range(400):
-        workloadArray.append(0)
+	workload = workLoadToDB4(transFormWorkload(filename))
+	#create a dictionairy <ID,value>
+	workloadArray = list()
+	#add 500 0 to workloadArray //shouldbechanged
+	for i in range(400):
+		workloadArray.append([i,0])
 
-    for line in workload:
-        ids = getIdsFormDB(line[1], connection)
-        for id in ids:
-            workloadArray[id[0]] += line[0]
-    return workloadArray
+	for line in workload:
+		ids = getIdsFormDB(line[1], connection)
+		for id in ids:
+			workloadArray[id[0]][1] += line[0]
+	return workloadArray
 
+
+# connection = openSQLITEDB('test.db')
+# test = fillDB4WorkloadData("workload.txt", connection)
+# for i in range(len(test)):
+# 	print(f"{i} : {test[i]}")
+
+
+#|----------------------------------------------------------------------------------------------------------------------|
+#| db3. Voor elke waarde van elk categorische datapunt, vindt log(QF(alles)/QF(dit))									|
+#|----------------------------------------------------------------------------------------------------------------------|
+"""def workLoadToDB3(catagorishDataPoint, workload):
+	#transform workload to "select catagorishDataPoint, count(id) from workload group by catagorishDataPoint;"
+	for line in workload:
+		whereClause = line[1].split('FROM autompg')[1]
+		whereClause = whereClause.replace(';','')
+		line[1] = f"SELECT {catagorishDataPoint} ,count(ID) FROM autompg {whereClause} group by {catagorishDataPoint};"
+	return workload
+
+def fillDB3WorkloadData(catagorishDataPoint, connection):
+	workload = workLoadToDB3(catagorishDataPoint, transFormWorkload("workload.txt"))
+	#create a list <catagorishDataPoint,amount of times mentioned>
+	workloadArray = list()
+	#create a list <catagorischdataPoint,<workloadarray>>
+	result = list()
+	cursor = connection.cursor()
+	cursor.execute(f"SELECT {catagorishDataPoint},count(ID) FROM autompg group by {catagorishDataPoint};")
+	catagorishDataPoints = cursor.fetchall()
+	print(catagorishDataPoints)
+	for line in catagorishDataPoints:
+		workloadArray.append([line[0], 0])
+	for line in workloadArray:
+
+
+
+	return result
+	
 connection = openSQLITEDB('test.db')
+test = fillDB3WorkloadData("brand", connection)"""
 
-test = fillDB4WorkloadData("workload.txt", connection)
-for i in range(len(test)):
-	print(f"{i} : {test[i]}")
 
 
 #print(transformCEQ("k = 9, model = ding, year = 1999, price = 50", "table"))
